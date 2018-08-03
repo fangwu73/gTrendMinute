b209a60fc18eb8ac146efc1d5b5231ab41a62034
Fang Wu <fangwu0703@gmai.com>
2018-08-02 17:36:19 -0700

    Added rescale functions and etc.

--- gTrendMinute.R
+++ gTrendMinute.R
@@ -1,6 +1,9 @@
+
+# Do NOT source the whole script. Run through all the functions before tests, then run the test one by one
 .pkgenv <- new.env(parent=emptyenv())
 
-# Get Google API cookies handler
+# Get Google API cookies handler ----
+
 get_api_cookies =
   #
   # get the requisite cookies from Google's API and assign cookie handler to .pkgenv
@@ -16,7 +19,7 @@ get_api_cookies =
   return(NULL)
 }
 
-# Get widget --------------------------------------------------------------
+# Get widget ----
 
 get_widget = 
   #
@@ -68,13 +71,14 @@ get_widget =
   }
 
 
-# Data over time ----------------------------------------------------------
+# Data over time ----
 
 interest_over_time =
   #
   # Function to fetch data frame: insterest over time
+  # pull the raw data in UTC/GMT
   #
-  function (widget, comparison_item, tz) {
+  function (widget, comparison_item) {
     # Prepare payload2 and URL by using widget
     payload2 = list()
     payload2$locale = widget$request$locale[1]
@@ -88,7 +92,7 @@ interest_over_time =
       "https://www.google.com/trends/api/widgetdata/multiline/csv?req=",
       jsonlite::toJSON(payload2, auto_unbox = T),
       "&token=", widget$token[1],
-      "&tz=0"
+      "&tz=0" #UTC
     ))
     
     # Get data back through the URL
@@ -132,14 +136,13 @@ interest_over_time =
       df$date = anytime::anydate(df$date, tz = 'GMT',asUTC = TRUE)
     } else {
       df$date = anytime::anytime(df$date, tz = 'GMT',asUTC = TRUE)
-      df$date = lubridate::with_tz(df$date,tz)
     }
     
     return(df)
   
 }
 
-# Check data interval -----------------------------------------------------
+# Utility functions ----
 
 checkTimeinterval =
   #
@@ -163,16 +166,26 @@ checkTimeinterval =
     return(widget$request$resolution)
   }
 
+naToZero = 
+  #
+  # NA/Inf to 0
+  #
+  function(x) {
+    x[is.na(x)] <- 0
+    x[!is.finite(x)] <- 0
+    return(x)
+  }
 
-# Sub main fun ------------------------------------------------------------
+# Sub main fun ----
 
 interest = 
   # 
-  # Main function to query google trend data in terms of interest over time 
+  # Main function to query google trend data in terms of interest over time in GMT/UTC 
   # Arg:
-  #    gprop: only be accepted single value 
+  #    gprop: only accepts single value 
+  #    time: e.g. "today+5-y", "2018-06-15T3 2018-06-15T7"
   # 
-  function (keyword, geo = "US", time = "today+5-y", gprop = c("web", "news", "images", "froogle", "youtube"), category = 0, hl = "en-US", tz) {
+  function (keyword, geo = "US", time = "today+5-y", gprop = c("web", "news", "images", "froogle", "youtube"), category = 0, hl = "en-US") {
     # check whether `gprop` matches any candidates
     gprop = match.arg(gprop, several.ok = FALSE)
     gprop = ifelse(gprop == "web", "", gprop)
@@ -188,20 +201,30 @@ interest =
                         hl)
     
     # Fetch data
-    interest_over_time = interest_over_time(widget, comparison_item, tz)
+    interest_over_time = interest_over_time(widget, comparison_item)
+    # interest_over_time$date = lubridate::with_tz(interest_over_time$date,tz)
+    
     return(interest_over_time)
   }
 
 
-# Main fun ----------------------------------------------------------------
+# Consecutive pull ----
 
-long_term_query_raw = 
+long_query_raw = 
   #
-  # Pull long term minute-level data with consecutive 4-hour window
-  # with 1 minute overlapping
+  # This function has more control than interest() with consecutive pulls with specified time windows
+  # Pull long term minute-level data with consecutive 4-hour window with 1 minute overlapping
+  # Pull consecutive 1 day/3 day/7 day window with corresponding resolution and overlapping records between windows
   # need to adjust due to rescaling issue with Google Trends
+  # Arg:
+  #    window: the time window you pull from Google Trends at once, the scale within this window is 0-100 and
+  #            the resolution depends on the window length.
+  #    tz: the timezone of start_date and end_date. e.g. "America/Los_Angeles". default "GMT".
+  # Output:
+  #    date: GMT/UTC
   #
-  function(start_date, end_date, keyword, geo = 'US', gprop = 'web', category = 0, hl = 'en-US', tz="America/Los_Angeles") {
+  function(start_date, end_date, window = c('4h'), keyword, geo = 'US', gprop = 'web', category = 0, hl = 'en-US', tz="GMT") {
+    # window = match.arg(arg = window, choices = c('4h','1d','3d','7d'), several.ok = FALSE)
     # require(parallel)
     # change date format
     start_date = strptime(
@@ -217,34 +240,70 @@ long_term_query_raw =
     # timezone in Google API is set to GMT, to confirm 
     start_date_GMT <- lubridate::with_tz(start_date, tzone = "GMT")
     end_date_GMT <- lubridate::with_tz(end_date, tzone = "GMT")
-    
+    window.length <- dplyr::case_when(
+      window =='4h' ~ '4 hour',
+      window =='1d' ~ '24 hour',
+      window =='3d' ~ '72 hour',
+      window =='7d' ~ '168 hour',
+      TRUE ~ 'NA')
     # create time stamp sequence
     diff.time = difftime(end_date_GMT, start_date_GMT, units = 'hours')
-    if(diff.time < 4) {
+    if (diff.time < 4) {
       time.stamp = seq(start_date_GMT, end_date_GMT, by = diff.time)
-    } else {
-      time.stamp = seq(start_date_GMT, end_date_GMT, by = '4 hour') 
-    }
-    
-    # create time pair
-    time.seq = paste(as.Date(time.stamp, 
-                             tz= attributes(time.stamp)$tzone), 
-                     lubridate::hour(time.stamp), 
-                     sep='T')
-    time.pair.1 = time.seq[-1]
-    time.pair.2 = time.seq[-length(time.seq)]
-    time.pair = paste(time.pair.2, time.pair.1)
-    
-    tmp.list <- vector('list', length = length(time.pair))
-    tmp.list <- lapply(time.pair,
-             function(x) {
-               interest(keyword = keyword, 
-                        geo = geo, 
-                        time = x, 
-                        gprop = gprop, 
-                        category = category,
-                        hl = hl,
-                        tz)})
+      } else {
+      time.stamp = seq(start_date_GMT, end_date_GMT, by = window.length) 
+      } 
+      # create time pair
+      time.seq = paste(as.Date(time.stamp, 
+                               tz= attributes(time.stamp)$tzone), 
+                       lubridate::hour(time.stamp), 
+                       sep='T')
+      time.pair.1 = time.seq[-1]
+      time.pair.2 = time.seq[-length(time.seq)]
+      time.pair = paste(time.pair.2, time.pair.1)
+      pb <- progress_estimated(length(time.pair))
+  
+      #tmp.list <- vector('list', length = length(time.pair))
+      rounds <- (length(time.pair)-1)%/%60
+      pb$tick()$print()
+      start.list <- list(interest(keyword = keyword,
+                             geo = geo,
+                             time = time.pair[1],
+                             gprop = gprop,
+                             category = category,
+                             hl = hl))
+      if (rounds>=1){
+        for(i in 1:rounds){
+          pb$tick()$print()
+          Sys.sleep(200)
+          
+          tmp.list <- vector('list', length = 60)
+          # print(time.pair[((i-1)*60+2):(i*60+1)])
+          tmp.list <- lapply(time.pair[((i-1)*60+2):(i*60+1)],
+                                                  function(x) {
+                                                    interest(keyword = keyword,
+                                                             geo = geo,
+                                                             time = x,
+                                                             gprop = gprop,
+                                                             category = category,
+                                                             hl = hl)}
+          )
+          start.list <- c(start.list, tmp.list)
+          
+        }
+      }
+      pb$tick()$print()
+      tmp.list <- lapply(time.pair[(rounds*60+2):length(time.pair)],
+                                                        function(x) {
+                                                          interest(keyword = keyword,
+                                                                   geo = geo,
+                                                                   time = x,
+                                                                   gprop = gprop,
+                                                                   category = category,
+                                                                   hl = hl)}
+      )
+      start.list <- c(start.list, tmp.list)
+      
     # paralle querying data
     # cl = makeCluster(4)
     # clusterExport(cl, varlist = c('interest', 'get_widget', 'interest_over_time'), envir = globalenv())
@@ -263,19 +322,187 @@ long_term_query_raw =
     # stopCluster(cl)
     
     # make result
-    tmpdf = do.call(rbind, tmp.list)
+    tmpdf = do.call(rbind, start.list)
     
     return(tmpdf)
     
   }
 
+# Daily window minute level - expect to run once and save it for future rescaling with longer windows ----
+daily_minute_gmt_raw = 
+  #
+  # Arg: tz is the timezone of start_date and end_date. e.g. "America/Los_Angeles". default "GMT". 
+  # Steps:
+  # 1. Pull long term minute-level data with consecutive 4-hour window with 1 minute overlapping
+  # 2. Pull consecutive 1-day window with 1 record overlapping
+  # 3. Rescale to get 1-day window minute level trend index
+  # Need to adjust again due to rescaling procedure with Google Trends
+  # Output: 
+  #     date: UTC/GMT 
+  #     raw hits and rescaled hits with 1 day window
+  #
+  function(start_date = '2018-01-01 00:00:00', end_date = '2018-08-01 00:00:00', keyword, 
+           geo = 'US', gprop = 'web', category = 0, hl = 'en-US', tz = "GMT") {
+    
+    
+    # timezone in Google API is set to GMT with tz=0 in the url 
+    start_date_GMT <- as.character(lubridate::with_tz(as.POSIXct(start_date), tzone = "GMT"))
+    end_date_GMT <- as.character(lubridate::with_tz(as.POSIXct(end_date), tzone = "GMT"))
+    
+    df_a <- long_query_raw(start_date = start_date_GMT, 
+                           end_date = end_date_GMT, 
+                           window = "4h",
+                           keyword = keyword, 
+                           geo = geo, 
+                           gprop = gprop, 
+                           category = category,
+                           hl = hl, 
+                           tz="GMT")
+    df_b <- long_query_raw(start_date = start_date_GMT, 
+                           end_date = end_date_GMT, 
+                           window = "1d",
+                           keyword = keyword, 
+                           geo = geo, 
+                           gprop = gprop, 
+                           category = category,
+                           hl = hl, 
+                           tz="GMT")
+    # df_a and df_b
+    resolution_a <- df_a$date[3] - df_a$date[2]
+    overlapping_a <- df_a %>% group_by(date, keyword, geo, gprop, category) %>% summarise(n=n()) %>% filter(n >1) %>% 
+      select(date, keyword, geo, gprop, category) %>% arrange(keyword, geo, gprop, category,date) %>% as.data.frame()
+    window_length_a <- ifelse(difftime(overlapping_a[2,1], overlapping_a[1,1], units = "hour") <= 24, 
+                              paste0(difftime(overlapping_a[2,1], overlapping_a[1,1], units = "hour"), "hours"),
+                              paste0(difftime(overlapping_a[2,1], overlapping_a[1,1], units = "day"), "days"))
+    resolution_b <- df_b$date[3] - df_b$date[2]
+    stopifnot(resolution_b > resolution_a)
+    if (is.na(window_length_a)) {
+      print("no need to rescale since the data is pulled from one window")
+      outputdf <- df_a
+    } else {
+      df_a_windows <- df_a %>% group_by(date, keyword, geo, gprop, category) %>% filter(duplicated(date) | n() == 1) %>% 
+        mutate(windows = lubridate::floor_date(date, unit = window_length_a))
+      
+      df_b_windows <- df_b %>% group_by(date, keyword, geo, gprop, category) %>% filter(duplicated(date) | n() == 1) %>% 
+        mutate(windows = lubridate::floor_date(date, unit = window_length_a))
+      scale_index <- inner_join(df_a_windows %>% group_by(keyword, geo, gprop, category, windows) %>% 
+                                  summarise(hits.avg.a = mean(hits, na.rm = TRUE)), 
+                                df_b_windows %>% group_by(keyword, geo, gprop, category, windows) %>% 
+                                  summarise(hits.avg.b = mean(hits, na.rm = TRUE))) %>% 
+        mutate(scale.index = naToZero(hits.avg.b/hits.avg.a))
+      outputdf <- df_a_windows %>% left_join(scale_index, by = c('keyword', 'geo', 'gprop', 'category', "windows")) %>%
+        mutate(hits.scaled = hits*scale.index) %>% as.data.frame() 
+    }
+    return(outputdf)
+  }
+
+# Final rescale from daily window to any longer window -  expected to run it frequently ----
+minute_gmt_rescale =
+  #
+  # Final rescale from daily window to any longer window
+  #
+  function(daily_minute_gmt_rawdf, hl='en-US') {
+    keywords <- unique(daily_minute_gmt_rawdf$keyword)
+    geos <- unique(daily_minute_gmt_rawdf$geo)
+    timespan <- paste(strptime(min(daily_minute_gmt_rawdf$date), format = "%Y-%m-%d", tz = "GMT"), 
+                      strptime(max(daily_minute_gmt_rawdf$date), format = "%Y-%m-%d", tz = "GMT"))
+    gprops <- unique(daily_minute_gmt_rawdf$gprop)
+    categories <- unique(daily_minute_gmt_rawdf$category)
+    
+    df_b <- interest(keyword = keywords, geo = geos, time = timespan, gprop = gprops, category = categories, hl = hl)
+    # plot(df_candidco_real$hits,type="l")
+    
+    resolution_a <- daily_minute_gmt_rawdf$date[3] - daily_minute_gmt_rawdf$date[2]
+    window_length_a <-'1day'
+    resolution_b <- df_b$date[3] - df_b$date[2]
+    stopifnot(resolution_b > resolution_a)
+    
+    df_a_windows <- daily_minute_gmt_rawdf %>% group_by(date, keyword, geo, gprop, category) %>% filter(duplicated(date) | n()==1) %>%
+      mutate(windows =lubridate::floor_date(date, unit = window_length_a))
+    df_b_windows <- df_b %>% group_by(date, keyword, geo, gprop, category) %>% filter(duplicated(date) | n()==1) %>%
+      mutate(windows = lubridate::floor_date(date, unit = window_length_a))
+    
+    scale_index_2 <- inner_join(df_a_windows %>% group_by(keyword, geo, gprop, category, windows) %>% summarise(hits.avg.scaled.a = mean(hits.scaled)),
+                                df_b_windows %>% group_by(keyword, geo, gprop, category, windows) %>% summarise(hits.avg.b.2 = mean(hits))) %>%
+      mutate(scale.index.2 = naToZero(hits.avg.b.2/hits.avg.scaled.a)) 
+    outputdf_2 <- df_a_windows %>% left_join(scale_index_2, by = c('keyword', 'geo', 'gprop', 'category',"windows")) %>% 
+      mutate(hits.scaled.2 = hits.scaled*scale.index.2) %>% as.data.frame()
+    return(outputdf_2)
+  }
 
 
-# test
-df = long_term_query_raw('2017-01-01 00:00:00', '2017-01-02 01:00:00', 
-                         keyword = c('zillow', 'trulia'), geo = c("US","CA"),tz="GMT")
+
+# Test 1 ----
+df_candid_co <- daily_minute_gmt_raw(start_date = '2018-04-23 00:00:00', 
+                                     end_date = '2018-05-02 00:00:00', 
+                                     keyword = "candid co", 
+                                     geo = 'US', 
+                                     gprop = 'web', 
+                                     category = 0, 
+                                     hl = 'en-US', 
+                                     tz = "GMT")
+# daily_minute_gmt_rawdf <- df_candid_co
+
+finaldf <- minute_gmt_rescale(daily_minute_gmt_rawdf = df_candid_co)
+plot(finaldf$hits.scaled.2, type = "l")
+lines(1:length(finaldf$hits.scaled), finaldf$hits.scaled, col = "blue")
+lines(1:length(finaldf$hits.avg.b.2), finaldf$hits.avg.b.2, col = "red")
+
+# Test 2 ----
+# multiple keywords, geo and a different timezone
+df = long_query_raw('2017-01-01 00:00:00', '2017-01-02 01:00:00', 
+                    keyword = c('zillow', 'trulia'), geo = c("US","CA"), tz = "America/Los_Angeles")
 attr(df$date,"tzone")
 
-df2 = long_term_query_raw(start_date = '2018-07-10 18:21:00', end_date = '2018-07-10 22:04:00', 
-                          keyword = c('groupon'), geo = c("US"), tz="America/Los_Angeles")
-attr(df2$date,"tzone")  
+# Test 3 ----
+# runtime tests
+a <- Sys.time()
+df_1 <- long_query_raw(start_date = '2018-04-23 00:00:00', end_date = '2018-05-02 00:00:00', 
+                     window="4h",
+                     keyword = c('23andme'), geo = c("US"), tz = "GMT")
+b <- Sys.time()
+b-a
+attr(df_1$date, "tzone")  
+plot(df_1$hits, type = "l")
+
+# Test 4 ----
+# 1 day window raw data
+a <- Sys.time()
+df_2 <- long_query_raw(start_date = '2018-01-01 00:00:00', 
+                       end_date = '2018-08-01 00:00:00', 
+                       window ="1d",
+                       keyword = c('groupon'), geo = c("US"))
+b <- Sys.time()
+b-a
+plot(df_2$hits,type="l")
+
+# Test 5 ----
+# 3 day window raw data and a different timezone
+df_4 = long_query_raw(start_date = '2018-06-01 00:00:00', 
+                      end_date = '2018-06-10 00:00:00', 
+                      window = "3d",
+                      keyword = c('groupon'), geo = c("US"), tz = "America/Los_Angeles")
+plot(df_4$hits,type = "l")
+tail(df_4)
+
+# Test 6 ----
+# output: minute level in GMT/UTC 
+daytest <- interest(keyword = "groupon", 
+                    geo = "US", 
+                    time = "2018-06-15T3 2018-06-15T7" , 
+                    gprop = 'web', category = 0, hl = 'en-US')
+head(daytest)
+
+# Test 7 ----
+# output: hour level in GMT/UTC 
+daytest_1 <- interest(keyword = "groupon", geo = "US", time = "2018-06-15T3 2018-06-18T7", 
+                      gprop = 'web', category = 0, hl = 'en-US')
+attr(daytest$date,"tzone") 
+
+# Test 8 ----
+# output: daily level in GMT/UTC 
+daytest_2 <- interest(keyword = c('groupon'), geo = "US", 
+                      time = "2018-06-01 2018-06-15", 
+                      gprop = c("web"), category = 0, hl = "en-US") 
+plot(daytest_2$hits,type="l")
+
